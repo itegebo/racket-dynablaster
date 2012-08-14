1d8c87dd801592c960771e4756417ea8ba6c43fb
Ian Tegebo <ian.tegebo@gmail.com>
2012-08-13 19:28:35 -0700

    Initial copy from local PLaneT install

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang setup/infotab
+(define name "Dynablaster")
+(define blurb
+  '("Simple Htdp version of Dynablaster game"))
+(define primary-file "dyna.rkt")
+(define categories '(media misc))
+(define scribblings '(("manual.scrbl")))
+(define version "1.13")
+(define release-notes
+  '((p "Game title customization")))
